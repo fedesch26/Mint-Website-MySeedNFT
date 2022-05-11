3b36ad9243ef3138027d4bcded534d6ee3f7292e
Fede-Dev <87677596+fedesch26@users.noreply.github.com>
2022-05-11 01:17:55 -0300

    update header

--- src/Header.js
+++ src/Header.js
@@ -28,8 +28,8 @@ const Header = () => {
           </span>
         </div>
       </a>
-      <div className="ml-10 hidden xl:flex h-12 w-12">
-        <div className="space-x-12">
+      <div className="ml-15 hidden xl:flex">
+        <div className="space-x-15">
 
         </div>
       </div>
